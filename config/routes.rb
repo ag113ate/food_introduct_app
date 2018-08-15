d2ed5fe00fecbb329fad61249a1fa35ed1327e0b
ag113ate <shota.terui@beach.ocn.ne.jp>
2018-08-15 08:21:53 +0000

    Create FoodMenues Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FoodIntroductApp::Application.routes.draw do
+  get "food_menues/top"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
